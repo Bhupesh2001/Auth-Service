1a9d46dec49b83e57ecdab29c7345fd2dd5344e4
Bhupesh <53337976+Bhupesh2001@users.noreply.github.com>
2025-06-29 12:15:53 +0530

    local started

---
+++ src/test/java/com/project/authService/AuthServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.authService;
+
+import authservice.AuthServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AuthServiceApplication.class)
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
